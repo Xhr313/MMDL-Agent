ee1d52eae2bf0f51830e4d0605a0bdd50279fe00
xhhhr <799882947@qq.com>
2026-02-05 10:14:14 +0800

    test

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from app.api.main import app
 # 终端运行: uvicorn main:app --reload
 # 访问地址: http://127.0.0.1:8000
 # 返回结果{"app":"industrial-anomaly-agent","version":"0.1.0","message":"Welcome to MMDL-Agent","docs":"/docs","openapi_schema":"/openapi.json"}
+# 1
